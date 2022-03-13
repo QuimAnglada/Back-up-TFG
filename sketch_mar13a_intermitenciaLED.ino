f7bd34bd3122c30b299033190a008453499d30ef
QuimAnglada <qimsins@gmail.com>
2022-03-13 11:32:44 +0100

    Create sketch_mar13a_intermitenciaLED.ino

---
+++ sketch_mar13a_intermitenciaLED.ino
@@ -0,0 +1,15 @@
+//Intermitència del LED
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT); // el primer argument s'ha declarat com a sortida.
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH); // encedre un pin digital, el pin és el primer argument, encendre és el segon (HIGH).
+  delay(1000); // Perquè hi ha intermitència, l'argument és el temps, expressat en ms, que la llum està apagada.  
+  digitalWrite(3, LOW); // apagar un pin digital (LOW).
+  delay(1000); 
+}
