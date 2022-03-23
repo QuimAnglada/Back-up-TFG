8c54f7dd83217820aa56cbcf73028eca758ca232
QuimAnglada <qimsins@gmail.com>
2022-03-23 19:54:19 +0100

    In process

--- LCD_HolaMundo/LCD_HolaMundo.ino
+++ LCD_HolaMundo/LCD_HolaMundo.ino
@@ -2,35 +2,48 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
-
-#define TFT_DC 7    
-#define TFT_CS 10   //Chip select
+#include "Board_Pinout.h"
+#include "Wire.h"
+#include "Adafruit_STMPE610.h"
 
 // Objecte amb nom tft, tipus Adafruit_ILI9341 i coma arguments passem els pins
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-
 void setup() {
+  Serial.print ("1");
+  delay(1000);
+  Serial.begin(115200);
+  Serial.print ("1aaa");
   // Inicialitzem la pantalla
   tft.begin();
-  // Orientació de la pantalla, els valors van de 0 a 3
-  tft.setRotation(0);
-  // Color de fons de tota la pantalla, ILI9341_seguit de color que ens interessi
-  tft.fillScreen(ILI9341_RED);
-  // Color del text
-  tft.setTextColor(ILI9341_WHITE);
-  // Mida del text, 6 px de base per 8 px, en el cas de dos 12 x 16 pxs.
-  tft.setTextSize(2);
-  // On situem el text dins de la pantalla, (x,-y)
-  tft.setCursor(0,0);
-  // Text
-  tft.print("Hola, han passat: ");
-  tft.setCursor(75,250);
-  tft.print("segons");
-}
+  Serial.print ("2");
+  Wire.begin();
+  pinMode(TFT_DC, OUTPUT);
+  delay(1000);
+ }
 
 void loop() {
-  tft.setTextColor(ILI9341_BLUE,ILI9341_RED);
+   // Orientació de la pantalla, els valors van de 0 a 3
+  tft.setRotation(0);
+  Serial.print ("3");
+  // Color de fons de tota la pantalla, ILI9341_seguit de color que ens interessi
+   tft.fillScreen(ILI9341_BLACK);
+   Serial.print ("4");
+  // Color del text
+  tft.setTextColor(ILI9341_WHITE);
+  Serial.print ("5");
+  // Mida del text, 6 px de base per 8 px, en el cas de dos 12 x 16 pxs.
+  tft.setTextSize(2);
+  Serial.print ("6");
+  // On situem el text dins de la pantalla, (x,-y)
+  tft.setCursor(0,0);
+  Serial.print ("7");
+  // Text
+  tft.print("Hola, han passat: ");
+  Serial.print ("8");
+  tft.setCursor(75,250);
+  tft.print("segons");
+  tft.setTextColor(ILI9341_WHITE,ILI9341_BLACK);
   tft.setTextSize(8);
   tft.setCursor(30,100);
   tft.print(millis()/1000);
