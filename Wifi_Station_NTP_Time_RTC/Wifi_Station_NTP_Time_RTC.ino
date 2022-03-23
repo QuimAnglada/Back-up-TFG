3963a50579dbde7c4ea10fcceec0b9651ff5038e
QuimAnglada <qimsins@gmail.com>
2022-03-23 19:57:34 +0100

    In process

---
+++ Wifi_Station_NTP_Time_RTC/Wifi_Station_NTP_Time_RTC.ino
@@ -0,0 +1,59 @@
+// Station -> Conexió al router de casa
+// NTP time -> mitjantçant la conexió wifi s'obté el temps real de qualsevol zona del món
+// RTC -> Real time clock, té com a objectiu mantenir el temps després de la configuració 
+//        inicial amb NTP (també funciona en cas de que no hi hagi conexió wifi)
+
+#include <WiFi.h>
+#include "time.h"
+
+// Replace with your network credentials (STATION)
+const char* ssid = "Enric";
+const char* password = "5a303030324346433132393633";
+
+// Direcció del servidor NTP que farem servir
+const char* ntpServer = "europe.pool.ntp.org";
+// Ajustem la nostra zona horàri, UTC+1.00 es correspon amb 3600 ms 
+const long  gmtOffset_sec = 3600;
+// Ajustem si compensem l'horari, és a dir, avancem i endarrerim el rellotge dos cops l'any. 3600 si ho fem, 0 si no.
+const int   daylightOffset_sec = 0;
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+  
+}
+
+void loop() {
+  delay(1000);
+  printLocalTime();
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+// Desconnectar i reconnectar a la wifi, possibles opcions a tenir en compte: 
+// https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino/#8
